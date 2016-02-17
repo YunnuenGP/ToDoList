5271465458ef1b45185b4446db4c6ca6761b1d23
YunnuenGP <walter.ygp@gmail.com>
2016-02-16 18:43:36 -0600

    adding style and the plus feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
 	var list = $('ul');
+
 	list.on('click', 'li', function() {
 		$(this).toggleClass('completed');
 	});
 
 	list.on('click', 'span', function(e) {
-		$(this).parent().fadeOut(350, function() {
+		$(this).parent().slideUp(350, function() {
 			$(this).remove();
 		});
 		e.stopPropagation();
@@ -13,10 +14,14 @@ $(document).ready(function() {
 
 	$('input[type="text"]').on('keypress', function(e) {
 		if(event.which === 13){
-			var todoText = $(this).val();
-			$('ul').append('<li><span>X</span> ' + todoText + '</li>');
-			$(this).val('');
+			var inputObj = $(this);
+			var todoText = inputObj.val();
+			$('ul').append('<li><span><i class="fa fa-trash"></i></span> ' + todoText + '</li>');
+			inputObj.val('');
 		}
 	});
-});
 
+	$('.fa-plus').on('click', function() {
+		$('input[type="text"]').slideToggle();
+	});
+});
